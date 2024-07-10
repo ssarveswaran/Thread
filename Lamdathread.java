ea79681eef46f36b5456395304879019e57434db
ssarveswaran <ssarveswaran89@gmail.com>
2024-07-10 15:54:59 +0530

    lamda

--- Lamdathread.java
+++ Lamdathread.java
@@ -1,1 +1,21 @@
 
+class Lamdathread{
+   public static void main(String args[]){
+  Runnable obj1=new Runnable() {
+    public void run(){
+        for(int i=0;i<50;i++){
+          System.out.println("Sankar");
+        }
+     }
+  };
+  Runnable obj2=()->{
+        for(int i=0;i<50;i++){
+          System.out.println("Sarvesh");
+        }
+     };
+     Thread t1=new Thread(obj1);
+     Thread t2=new Thread(obj2);
+   t1.start();
+   t2.start();
+   }
+}
